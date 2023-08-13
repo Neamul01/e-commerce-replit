58bdad4f2f267f4a7c40abb4f757f6ea7009589a
Neamul01 <neamulhaque2002@gmail.com>
2023-08-13 13:28:10 +0600

    table updated

--- app/admin/tables/page.tsx
+++ app/admin/tables/page.tsx
@@ -1,7 +1,10 @@
+"use client";
 import React from "react";
 
 import CardTable from "@/components/Admin/Cards/CardTable";
 import Admin from "@/components/Layouts/Admin";
+import { AiFillEdit } from "react-icons/ai";
+import Table from "@/components/Common/Table";
 
 export default function Tables() {
   return (
@@ -12,8 +15,90 @@ export default function Tables() {
         </div>
         <div className="w-full mb-12 px-4">
           {/* <CardTable color="dark" /> */}
+          <Table columns={tableColumns} data={tableData} />
         </div>
       </div>
     </Admin>
   );
 }
+
+const tableColumns = [
+  {
+    id: 1,
+    label: "Product list",
+    value: "productList",
+  },
+  {
+    id: 2,
+    label: "Product code",
+    value: "productCode",
+  },
+  {
+    id: 3,
+    label: "Quantity",
+    value: "quantity",
+  },
+  {
+    id: 4,
+    label: "Price",
+    value: "price",
+  },
+  {
+    id: 5,
+    label: "Amount",
+    value: "amount",
+  },
+  {
+    id: 6,
+    label: "Status",
+    value: "status",
+  },
+  {
+    label: "Action",
+    content: (item: any) => {
+      return (
+        <div>
+          <AiFillEdit onClick={() => console.log(item)} />
+        </div>
+      );
+    },
+  },
+];
+
+const tableData = [
+  {
+    id: 1,
+    productList: "Argon Design System",
+    productCode: "ADS-1234567",
+    quantity: "1",
+    price: "$2,500 USD",
+    amount: "$2,500 USD",
+    status: "pending",
+  },
+  {
+    id: 2,
+    productList: "Angular Now UI Kit PRO",
+    productCode: "ANK-1234567",
+    quantity: "2",
+    price: "$1,800 USD",
+    amount: "$3,600 USD",
+    status: "completed",
+  },
+];
+
+type Column<T> = {
+  id: string | number;
+  label: string;
+  value: keyof T; // Use keyof T to ensure 'value' refers to a valid property of T
+  content?: (item: T) => JSX.Element;
+};
+
+type TableData<T> = {
+  id: string | number;
+  productList: string;
+  productCode: string;
+  quantity: string;
+  price: string;
+  amount: string;
+  status: string;
+};
--- components/Common/Table.tsx
+++ components/Common/Table.tsx
@@ -1,37 +1,44 @@
 import React from "react";
 
-export default function Table({
-  columns,
-  data,
-}: {
+type Column = {
+  id?: string | number;
+  label: string;
+  value?: string;
+  content?: (item: any) => JSX.Element;
+};
+
+type TableData = {
+  id: string | number;
+  [key: string]: any;
+};
+
+type TableProps = {
   columns: Column[];
-  data: Data[];
-}) {
+  data: TableData[];
+};
+
+export default function Table({ columns, data }: TableProps) {
   return (
     <div className="relative overflow-x-auto">
       <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            {columns.map((column, i) => (
-              <th
-                key={column.id + column.label}
-                scope="col"
-                className="px-6 py-3"
-              >
+            {columns.map((column, index) => (
+              <th key={column.id || index} scope="col" className="px-6 py-3">
                 {column.label}
               </th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {data.map((item, i) => (
+          {data.map((row) => (
             <tr
-              key={i}
+              key={row.id}
               className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
             >
-              {columns.map((column) => (
-                <td key={column.id + column.label} className="px-6 py-4">
-                  {column.content ? column.content(item) : item[column.value]}
+              {columns.map((column, columnIndex) => (
+                <td key={column.id || columnIndex} className="px-6 py-4">
+                  {column.content ? column.content(row) : row[column.value]}
                 </td>
               ))}
             </tr>
@@ -41,15 +48,3 @@ export default function Table({
     </div>
   );
 }
-
-type Column = {
-  id: string | number;
-  label: string;
-  value?: string;
-  content?: (item: any) => JSX.Element;
-};
-
-type Data = {
-  id: string | number;
-  content: (item: any) => JSX.Element;
-};
