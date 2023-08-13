9b88bc3afac3fc63ac43cd404c0c1bdfccc63ac7
Neamul01 <neamulhaque2002@gmail.com>
2023-08-13 15:07:52 +0600

    single product added

--- app/admin/products/[id]/page.tsx
+++ app/admin/products/[id]/page.tsx
@@ -1,62 +1,120 @@
+"use client";
+import { Carousel, Rating, Tabs } from "flowbite-react";
 import Image from "next/image";
 import React from "react";
+import { HiAdjustments, HiClipboardList, HiUserCircle } from "react-icons/hi";
+import { MdDashboard } from "react-icons/md";
 
 export default function page() {
   return (
-    <div className="container mx-auto py-8 min-h-screen">
+    <div className="container mx-auto py-8 min-h-screen flex flex-col gap-4">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         {/* Left section - Product Image */}
         <div className="mb-4 md:mb-0 h-[400px] w-[640px] relative">
-          <Image
+          {/* <Image
             src="https://e-commerce-luxify.vercel.app/_next/image?url=https%3A%2F%2Fswiperjs.com%2Fdemos%2Fimages%2Fnature-1.jpg&w=1920&q=75"
             alt="Product"
             fill
             className="w-full h-auto"
-          />
+          /> */}
+          <Carousel>
+            <img
+              alt="..."
+              src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+            />
+            <img
+              alt="..."
+              src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+            />
+            <img
+              alt="..."
+              src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+            />
+            <img
+              alt="..."
+              src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
+            />
+            <img
+              alt="..."
+              src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
+            />
+          </Carousel>
         </div>
 
         {/* Right section - Product Description */}
-        <div>
-          <h1 className="text-2xl font-semibold mb-2">Product Name</h1>
-          <p className="text-gray-600 mb-4">Product Description...</p>
-
-          {/* Product Tabs */}
-          <div className="border-b border-gray-300 mb-4">
-            <div className="flex">
-              <button className="py-2 px-4 font-semibold border-b-2 border-transparent hover:border-blue-500 focus:outline-none">
-                Description
-              </button>
-              <button className="py-2 px-4 font-semibold border-b-2 border-transparent hover:border-blue-500 focus:outline-none">
-                Specifications
-              </button>
-              <button className="py-2 px-4 font-semibold border-b-2 border-transparent hover:border-blue-500 focus:outline-none">
-                Reviews
-              </button>
-            </div>
+        <div className="flex flex-col gap-4">
+          <h1 className="text-2xl font-semibold mb-2">Fashionable Watch</h1>
+          <div className="text-3xl font-bold">$46</div>
+          <div className="flex gap-4">
+            <Rating>
+              <Rating.Star />
+              <Rating.Star />
+              <Rating.Star />
+              <Rating.Star />
+              <span className="text-gray-600 ml-2">(3.5)</span>
+            </Rating>
+            <p className="">(1)Reviews</p>
           </div>
-
-          {/* Tab Content */}
-          <div>
-            {/* Description Tab */}
+          <div className="flex justify-between">
             <div>
-              <h2 className="text-lg font-semibold mb-2">Description</h2>
-              <p className="text-gray-600">Detailed product description...</p>
+              <p className="">
+                Available Items: <span className="font-bold">34</span>{" "}
+              </p>
+              <p className="">
+                Sold: <span className="font-bold">22</span>{" "}
+              </p>
             </div>
-
-            {/* Specifications Tab */}
-            <div className="hidden">
-              <h2 className="text-lg font-semibold mb-2">Specifications</h2>
-              {/* Add specifications content here */}
-            </div>
-
-            {/* Reviews Tab */}
-            <div className="hidden">
-              <h2 className="text-lg font-semibold mb-2">Reviews</h2>
-              {/* Add reviews content here */}
+            <div>
+              <p className="">
+                Comission given: <span className="font-bold">$2</span>{" "}
+              </p>
+              <p className="">
+                Ordered: <span className="font-bold">34</span>{" "}
+              </p>
             </div>
           </div>
+          <p className="text-gray-600 mb-4">
+            Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae
+            luctus metus libero eu augue. Morbi purus liberpuro ate vol faucibus
+            adipiscing.
+          </p>
         </div>
       </div>
+      <Tabs.Group aria-label="Default tabs" style="default">
+        <Tabs.Item active icon={HiUserCircle} title="Description">
+          <p>
+            This is
+            <span className="font-medium text-gray-800">
+              Profile tab&apos;s associated content
+            </span>
+            . Clicking another tab will toggle the visibility of this one for
+            the next. The tab JavaScript swaps classes to control the content
+            visibility and styling.
+          </p>
+        </Tabs.Item>
+        <Tabs.Item icon={MdDashboard} title="Information">
+          <p>
+            This is
+            <span className="font-medium text-gray-800">
+              Dashboard tab&apos;s associated content
+            </span>
+            . Clicking another tab will toggle the visibility of this one for
+            the next. The tab JavaScript swaps classes to control the content
+            visibility and styling.
+          </p>
+        </Tabs.Item>
+        <Tabs.Item icon={HiAdjustments} title="Reviews">
+          <p>
+            This is
+            <span className="font-medium text-gray-800">
+              Settings tab&apos;s associated content
+            </span>
+            . Clicking another tab will toggle the visibility of this one for
+            the next. The tab JavaScript swaps classes to control the content
+            visibility and styling.
+          </p>
+        </Tabs.Item>
+      </Tabs.Group>
     </div>
   );
 }
